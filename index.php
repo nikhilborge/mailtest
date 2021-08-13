ede30e36ee7abd5099041ad6de3e2c4c83af3eeb
nikhil borge <nikhilb3439.nb@gmail.com>
2021-08-13 14:34:34 +0530

    bootstrap added

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 
-<html>
+<!-- <html>
     <body>
-        <form method="post" action="<?php echo $_SERVEr['PHP_SELF']; ?>">
+        <form method="post" action="">
         Email: <input name="email" type="text"/> <br/><br/>
         Subject: <input name="subject" type="text"/><br/><br/>
         Message:<br/>
@@ -10,12 +10,86 @@
         <input type="submit" type="submit"/>
         </form>
     </body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<script src="/index.js"></script>
+    <title>Form</title>
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid mt-3 padding">
+<form action="<?php echo $_SERVEr['PHP_SELF']; ?>" method="post">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" name="subject">
+  </div>
+ 
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Message</label>
+    <input type="text" class="form-control" id="exampleInputPassword1" name="message">
+  </div>
+
+  <button type="submit" class="btn btn-primary" name="submit">Sent</button>
+</form>
+</div>
+
+</body>
 </html>
 
 
 
 <?php
-if(isset($_POST['esubmit'])){
+if(isset($_POST['submit'])){
     $to ="nikhilborge3439.nb@gmail.com";
     $subject = $_POST['subject'];
     $message = $_POST['message'];
@@ -26,7 +100,10 @@ if(isset($_POST['esubmit'])){
 
 mail($to, $subject, $message, $headers);
 
-echo "Mail Sent."
+echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+<strong>Success</strong> Your email '.$email.' and password '.$password.' has been submitted successfully.
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>'
 
 ?>
 
